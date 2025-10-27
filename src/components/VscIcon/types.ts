d9b6ac28fda8c510c76e98081e0363c1203d5f36
s0u1drinker <norkiw40k@gmail.com>
2025-10-27 14:21:40 +0300

    Для компонента <VscIcon>: 1. Добавлена возможность задать цвет; 2. Установлен атрибут <aria-hidden> (по умолчанию <true>), который можно изменить с помощью props. Добавленыы тесты для проверки цвета и атрибута <aria-hidden>.

--- src/components/VscIcon/types.ts
+++ src/components/VscIcon/types.ts
@@ -1,7 +1,21 @@
-type IconSize = `${number}px` | `${number}%` | `${number}rem` | `${number}em`
-export type IconName = `${any}${string}:${any}${string}`
+type IconSize = `${number}px` | `${number}%` | `${number}rem` | `${number}em`;
+export type IconName = `${any}${string}:${any}${string}`;
 
 export interface IconProps {
-  iconName: IconName,
-  size?: IconSize,
+  /**
+   * Наименование иконки.
+   */
+  iconName: IconName;
+  /**
+   * Размер иконки (опционально). По умолчанию "1rem".
+   */
+  size?: IconSize;
+  /**
+   * Цвет иконки (опционально).
+   */
+  iconColor?: string;
+  /**
+   * Атрибут "aria-hidden" (опционально). По умолчанию "true".
+   */
+  ariaHidden?: boolean;
 }
